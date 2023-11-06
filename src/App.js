f670d5b21eb8c38623cbff3a12278cb8c8c4d93a
isunilchavan <thesunilchavan@gmail.com>
2023-11-06 11:58:08 +0530

    login and forgot Password

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Navigation from "./components/Navigation";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/forgot-password" element={<ForgotPassword />} /> 
         </Routes>
       </div>
     </Router>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>
+          <h2>Welcome to your mail box</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Dashboard;
